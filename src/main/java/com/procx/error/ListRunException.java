f7c045ef49f1a170efeb0392a740e33682ed931a
goodprocx <39480286+goodprocx@users.noreply.github.com>
2018-08-16 10:13:23 +0800

    11111111111111

--- src/main/java/com/procx/error/ListRunException.java
+++ src/main/java/com/procx/error/ListRunException.java
@@ -18,7 +18,7 @@ public class ListRunException {
 		//修改次数的记录作用：就是防止多线程，某个线程在迭代的时候，list的结构发生变法，因为ArrayList是非线程安全的
 		for (Iterator<String> it = list.iterator(); it.hasNext();) { 
             String val = it.next();
-            if (val.equals("c")) {
+            if (val.equals("b")) {
 //                list.add(val); //报错
                 list.remove(val);   //报错    
             }
